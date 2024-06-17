4a81033ce7b465c3074b47a53f5302edc820784d
Astalum <taaro20040526@icloud.com>
2024-06-17 13:14:46 +0900

    再起動

--- main.py
+++ main.py
@@ -24,28 +24,10 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    # # ユーザーからのメンションを受け取った場合、everyoneメンションを返す
-    # if client.user in message.mentions:
-    #     answer = "@everyone"
-    #     print(answer)
-    #     await message.channel.send(answer)
-
-    # # ユーザーからのメンションを受け取った場合、そのメッセージにリアクションをつける
-    # if client.user in message.mentions:
-    #     emoji = "<:delay:1224985952393625630>"
-    #     await message.add_reaction(emoji)
-
-    # ユーザーからのメンションを受け取った場合、そのメッセージの内容を取得する
-    # if client.user in message.mentions:
-    #     contents = message.content
-        # print(message.mentions[0].bot)
-        # for n in range(len(message.mentions)):
-        #     if message.mentions[n].bot == True:
-        #         print(contents)
-        #         user_id="<@"+str(message.mentions[0].id)+">"
-        #         print(user_id)
-        #         contents.replace(user_id,'')
-        # await message.channel.send(contents)
+    # ユーザーからのメンションを受け取った場合、そのメッセージにリアクションをつける
+    if client.user in message.mentions:
+        emoji = "<:delay:1224985952393625630>"
+        await message.add_reaction(emoji)
 
 # Bot起動
 client.run(config.DISCORD_TOKEN)
