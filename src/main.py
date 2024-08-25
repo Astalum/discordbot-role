00c740f53a1412c0bb83c2872b28b66334898212
Astalum <taaro20040526@icloud.com>
2024-08-25 21:34:16 +0900

    スレッドの名前を投稿の内容にするように変更

--- src/main.py
+++ src/main.py
@@ -10,10 +10,13 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
 # dockercontainer用
-path_json = "/app/src/reactions.json"
+# path_json = "/app/src/reactions.json"
+# path_txt = "/app/src/id.txt"
 # local用
-path_json = "/home/astalum/discordbot/konsei/discordbot-attend/reactions.json"
+path_json = "./reactions.json"
+path_txt = "./id.txt"
 write_json = False
+write_txt = False
 
 
 # bot起動時に発火
@@ -21,9 +24,11 @@ write_json = False
 async def on_ready():
     print("bot is online!")
     global write_json
+    global write_txt
     # アクティビティを設定
     new_activity = f"出欠席リアクション"
     write_json = False
+    write_txt = False
     await client.change_presence(activity=discord.Game(name=new_activity))
     # スラッシュコマンドを同期
     await tree.sync()
@@ -33,6 +38,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     global write_json
+    global write_txt
     global reaction_num
     global path_json
     # 自身が送信したメッセージには反応しない
@@ -41,18 +47,23 @@ async def on_message(message):
 
     # ユーザーからのメンションを受け取った場合、そのメッセージにリアクションをつけ、スレッドを作る
     if client.user in message.mentions:
-        with open(path_json, "r") as f_r:
-            reaction_dict = json.load(f_r)
+        with open(path_json, "r") as f_json:
+            reaction_dict = json.load(f_json)
         reaction_list = list(reaction_dict)
+        with open(path_txt, "r") as f_txt:
+            len_id = len(f_txt.read())
+            # print(len_id)
+        len_id += 4
+        thread_name = message.content[len_id:]
         channel = message.channel
+        # print(thread_name)
         thread = await channel.create_thread(
-            name=message, type=discord.ChannelType.public_thread
+            name=thread_name, message=message, type=discord.ChannelType.public_thread
         )
         await thread.send("遅刻・欠席・その他連絡はこちらから！")
         for emoji in reaction_list:
             emoji_id = "<:" + emoji + ":" + reaction_dict[emoji] + ">"
             await message.add_reaction(emoji_id)
-
     # JSONファイルへの書き込み
     if write_json == True:
         with open(path_json, "r") as f_r:
@@ -60,8 +71,9 @@ async def on_message(message):
         reaction_list = list(reaction_dict)
         # print(reaction_dict)
         if reaction_num + 2 > len(reaction_list):
+            write_json = False
             await message.channel.send(
-                "/finished コマンドを実行して更新作業を終了してください"
+                "出欠席リアクションの設定を終了しました。@メンションをして正しく設定されているかを確認してください。"
             )
         else:
             await message.channel.send(reaction_list[reaction_num + 1])
@@ -72,10 +84,16 @@ async def on_message(message):
             with open(path_json, "w") as f_w:
                 json.dump(reaction_dict, f_w, indent=4)
         # print(reaction_dict)
+    # TXTファイルへの書き込み
+    if write_txt == True:
+        with open(path_txt, "w") as f_w:
+            f_w.write(message.content)
+        write_txt = False
+        await message.channel.send("アプリIDの設定が完了しました")
 
 
 @tree.command(
-    name="update_reactions-id", description="出欠席リアクションIDを更新します"
+    name="update_reactions-id", description="出欠席リアクションIDを設定します"
 )
 async def start_update_reaction(interaction: discord.Interaction):
     global write_json
@@ -83,16 +101,16 @@ async def start_update_reaction(interaction: discord.Interaction):
     write_json = True
     reaction_num = 0
     await interaction.response.send_message(
-        "出欠席リアクションのIDを更新します。リアクションに対応するものを返信してください。\nSoprano_attend"
+        "出欠席リアクションのIDを設定します。リアクションに対応するものを返信してください。\nSoprano_attend"
     )
 
 
-@tree.command(name="finished", description="出欠席リアクションIDの更新を終了します")
+@tree.command(name="update_bot-id", description="botのアプリIDを設定します")
 async def finish_update_reaction(interaction: discord.Interaction):
-    global write_json
-    write_json = False
+    global write_txt
+    write_txt = True
     await interaction.response.send_message(
-        "出欠席リアクションの更新を終了しました。@メンションをして正しく設定されているかを確認してください。"
+        "botのアプリIDを設定します。アプリIDを返信してください。"
     )
 
 
