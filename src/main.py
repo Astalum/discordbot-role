b0f5cab208374b9acd4cb4b835888f9e39382ac3
Astalum <taaro20040526@icloud.com>
2024-08-25 17:15:48 +0900

    スレッドの名前変更

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
 import discord
 import config
 from discord import app_commands
+
 # import re
 import json
 
 intents = discord.Intents.all()
-client=discord.Client(intents=intents)
+client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-path_json = '/app/src/reactions.json'
-write_json=False
+path_json = "/app/src/reactions.json"
+write_json = False
+
 
 # bot起動時に発火
 @client.event
@@ -23,6 +25,7 @@ async def on_ready():
     # スラッシュコマンドを同期
     await tree.sync()
 
+
 # メッセージの検知
 @client.event
 async def on_message(message):
@@ -35,49 +38,59 @@ async def on_message(message):
 
     # ユーザーからのメンションを受け取った場合、そのメッセージにリアクションをつけ、スレッドを作る
     if client.user in message.mentions:
-        with open(path_json,'r') as f_r:
+        with open(path_json, "r") as f_r:
             reaction_dict = json.load(f_r)
         reaction_list = list(reaction_dict)
-        channel =message.channel
-        thread = await channel.create_thread(name="諸連絡", message=message, type=discord.ChannelType.public_thread)
+        channel = message.channel
+        thread = await channel.create_thread(
+            name=message, message=message, type=discord.ChannelType.public_thread
+        )
         await thread.send("遅刻・欠席・その他連絡はこちらから！")
         for emoji in reaction_list:
-            emoji_id="<:"+emoji+":"+reaction_dict[emoji]+">"
+            emoji_id = "<:" + emoji + ":" + reaction_dict[emoji] + ">"
             await message.add_reaction(emoji_id)
 
     # JSONファイルへの書き込み
     if write_json == True:
-        with open(path_json,'r') as f_r:
+        with open(path_json, "r") as f_r:
             reaction_dict = json.load(f_r)
         reaction_list = list(reaction_dict)
         # print(reaction_dict)
         if reaction_num + 2 > len(reaction_list):
-            await message.channel.send("/finished コマンドを実行して更新作業を終了してください")
+            await message.channel.send(
+                "/finished コマンドを実行して更新作業を終了してください"
+            )
         else:
             await message.channel.send(reaction_list[reaction_num + 1])
         if reaction_num <= len(reaction_list):
             dict_key = reaction_list[reaction_num]
             reaction_dict[dict_key] = message.content
             reaction_num += 1
-            with open(path_json,'w') as f_w:
+            with open(path_json, "w") as f_w:
                 json.dump(reaction_dict, f_w, indent=4)
         # print(reaction_dict)
 
 
-@tree.command(name="update_reactions-id", description="出欠席リアクションIDを更新します")
+@tree.command(
+    name="update_reactions-id", description="出欠席リアクションIDを更新します"
+)
 async def start_update_reaction(interaction: discord.Interaction):
     global write_json
     global reaction_num
     write_json = True
     reaction_num = 0
-    await interaction.response.send_message("出欠席リアクションのIDを更新します。リアクションに対応するものを返信してください。\nSoprano_attend")
+    await interaction.response.send_message(
+        "出欠席リアクションのIDを更新します。リアクションに対応するものを返信してください。\nSoprano_attend"
+    )
 
 
 @tree.command(name="finished", description="出欠席リアクションIDの更新を終了します")
 async def finish_update_reaction(interaction: discord.Interaction):
     global write_json
     write_json = False
-    await interaction.response.send_message("出欠席リアクションの更新を終了しました。@メンションをして正しく設定されているかを確認してください。")
+    await interaction.response.send_message(
+        "出欠席リアクションの更新を終了しました。@メンションをして正しく設定されているかを確認してください。"
+    )
 
 
 # Bot起動
